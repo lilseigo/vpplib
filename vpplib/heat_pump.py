45ba02f0e93913cc0c96e13d74f6bd05ec78941f
Pyosch <birk-sascha@web.de>
2021-03-24 09:33:13 +0100

    Moved optimize_bivalent to heat_pump.py

--- vpplib/heat_pump.py
+++ vpplib/heat_pump.py
@@ -599,3 +599,84 @@ class HeatPump(Component):
         self.el_power = round(
             float(self.th_power
                   / self.get_current_cop(dataframe['temperature'][0])), 1)
+
+    def optimize_bivalent(self, heating_rod, mode, norm_temp, user_profile):
+        """.
+
+        Parameters
+        ----------
+        heating_rod : TYPE
+            DESCRIPTION.
+        mode : TYPE
+            DESCRIPTION.
+        norm_temp : TYPE
+            DESCRIPTION.
+        user_profile : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        if mode not in ["parallel", "alternative"]:
+            print("error: mode needs to be \"parallel\" or \"alternative\"")
+
+    # =============================================================================
+    #     if type(heat_pump) != HeatPump:
+    #         print("error: heat_pump needs to be of type HeatPump")
+    #
+    #     if type(heating_rod) != HeatingRod:
+    #         print("error: heating_rod needs to be of type HeatingRod")
+    # =============================================================================
+
+    # =============================================================================
+    #     if user_profile.thermal_energy_demand == None:
+    #         user_profile.get_thermal_energy_demand()
+    # =============================================================================
+
+        if norm_temp <= -16:
+            biv_temp = -4
+        elif (norm_temp > -16) & (norm_temp <= -10):
+            biv_temp = -3
+        elif norm_temp > -10:
+            biv_temp = -2
+
+        heat_demand = self.user_profile.get_thermal_energy_demand()
+        temperature = pd.read_csv(
+            "./input/thermal/dwd_temp_15min_2015.csv", index_col="time")
+
+        # get point p0 (lowest temperature and corresponding (highest) heat demand)
+        T_p0 = round(float(temperature['temperature'].min()), 1)
+        P_p0 = round(float(heat_demand['thermal_energy_demand'].max()), 1)
+
+        # get point p1 (heatstop temperature (20°C) and corrsponding (0kW) heat demand)
+        T_p1 = 20   # choose reasonable value
+        P_p1 = round(float(heat_demand['thermal_energy_demand'].min()), 1)
+
+        # assume linear function P(T)=a*T+b between p0 and p1
+        # calculate parameter a: gradient triangle
+        a = (P_p1 - P_p0) / (T_p1 - T_p0)
+
+        # calculate parameter b: b=P(T)-a*T
+        b = 0 - a * 20  # Annahme bei 20 Grad keine Energie für Raumwärme notwendig
+
+        # bivalence temerature (determine with tabels in Vaillant hand book)
+        T_biv = biv_temp
+
+        # calculate corresponding heat demand (equals thermal power of heat pump)
+        P_biv = a * T_biv + b
+        # print(str(P_biv))
+        self.th_power = round(float(P_biv), 1)  # * 1.3
+
+        self.el_power = self.th_power / self.get_current_cop(T_biv)
+        th_power_hp_coldest = self.el_power * self.get_current_cop(norm_temp)
+
+        if mode == "parallel":
+            heating_rod.el_power = round(
+                float((P_p0 - th_power_hp_coldest)
+                      / heating_rod.efficiency), 1)
+
+        else:
+            heating_rod.el_power = round(
+                float(P_p0 / heating_rod.efficiency), 1)
