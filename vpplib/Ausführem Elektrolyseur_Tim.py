90de40f91040a7e407978de418533a799c8aa5b6
Timrie1812 <Timrieken1812@gmail.com>
2023-11-28 15:13:16 +0100

    test

--- vpplib/Ausführem Elektrolyseur_Tim.py
+++ vpplib/Ausführem Elektrolyseur_Tim.py
@@ -3,36 +3,31 @@ from a_hydrogen_elec_copy_Tim import ElectrolysisMoritz #importieren der klasse
 
 #TODO: wöfür die +40 bei H20
 
+#Import der Eingangsleistung
 
-#import windenergy timeseries
-
-
-#ts=pd.read_csv(r'C:\Users\Anwender\Documents\Masterprojekt\12345\vpplib\vpplib\wind.csv', sep=',', decimal='.',nrows=29)#, header=3
 ts = pd.read_csv(r'C:\Users\Anwender\Documents\Masterprojekt\12345\vpplib\vpplib\a_wind_energy_cologne.csv', sep=',', decimal='.',nrows=20)
 #ts = pd.read_csv(r"C:\Users\katri\vpplib\vpplib\a_wind_energy_cologne.csv", sep=',', decimal='.',nrows=100)
-#ts = pd.read_csv('a_wind_energy_cologne.csv',sep=',', decimal='.',nrows=100)
+#ts = pd.read_csv('a_wind_energy_cologne.csv',sep=',', decimal='.',nrows=20)
+
+
 
 #ts['P_in [KW]'] = round(ts['P_in [KW]']/100,2)
 ts['P_ac'] = round(ts['P_ac']/100,2)
 #print(ts)
-ts['time']=0
+#ts['time']=0
 
 
 #definieren eines elektrolyseurs mit der Klasse Electrolyzer
-electrolyzer = ElectrolysisMoritz(1,"mW",350,15,"s",300,'g')  #kw,Einheit P, bar, dt, Einheit zeit, benötigte Wasserstoffmenge, Einheit Wasserstoffmenge
+electrolyzer = ElectrolysisMoritz(500,"kW",350,15,"m",5,'kg')  #Elektrolyseur-Größe,Einheit Elektrolyseur, bar, dt, Einheit zeit, benötigte Wasserstoffmenge, Einheit Wasserstoffmenge
 
-#Auführen von Funktionen
-#Hier Wasserstoffproduktion
-
+#Auführen des Elektrolyseurs
 electrolyzer.prepare_timeseries(ts)
-
 print(ts)
+
+#CSV-Datei
 ts.to_csv(r'C:\Users\Anwender\Documents\Masterprojekt\12345\vpplib\vpplib\a_output.csv', index=False)
-
-#print(electrolyzer.create_polarization().iloc[504,0])
-#print(electrolyzer.stack_nominal())
-
+#ts.to_csv('a_output.csv', index=False)
+#EXCEL-Datei
 excel_file_path = r'C:\Users\Anwender\Documents\Masterprojekt\12345\vpplib\vpplib\a_output.xlsx'
 ts.to_excel(excel_file_path, index=False)
-#ts.to_csv('a_output.csv', index=False)
-#output ist eine timeseries mit zwei neuen Zeilen, Statuscodes und hydrogen production
+
